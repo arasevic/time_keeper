4281b678dc0df9a850e96f9f65d7389e1420f899
Andrej Rasevic <andrej@rasevicengineering.com>
2016-07-25 17:38:49 -0400

    added enum to employee role field

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,12 +2,14 @@ class Employee < ApplicationRecord
 	has_secure_password
 	has_many :pay_periods
 	belongs_to :work_group
-	
+
 	accepts_nested_attributes_for :pay_periods
 
+	enum role: [:admin, :contractor]
+	
 	def self.authenticate(email, password)
 		employee = Employee.find_by(email: email)
 		employee && employee.authenticate(password)
 	end
-	
+
 end
